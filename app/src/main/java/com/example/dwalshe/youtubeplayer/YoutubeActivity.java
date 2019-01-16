7ef304ca7f9a171c9a3b1a97e3fa511c6de421bf
dwalshe <david.walshe93@gmail.com>
2019-01-16 19:01:53 +0000

    - Added dynamically a Youtube Player Widget in code

--- app/src/main/java/com/example/dwalshe/youtubeplayer/YoutubeActivity.java
+++ app/src/main/java/com/example/dwalshe/youtubeplayer/YoutubeActivity.java
@@ -1,10 +1,13 @@
 package com.example.dwalshe.youtubeplayer;
 
 import android.os.Bundle;
+import android.support.constraint.ConstraintLayout;
+import android.view.ViewGroup;
 
 import com.google.android.youtube.player.YouTubeBaseActivity;
 import com.google.android.youtube.player.YouTubeInitializationResult;
 import com.google.android.youtube.player.YouTubePlayer;
+import com.google.android.youtube.player.YouTubePlayerView;
 
 public class YoutubeActivity extends YouTubeBaseActivity
         implements YouTubePlayer.OnInitializedListener {
@@ -12,7 +15,14 @@ public class YoutubeActivity extends YouTubeBaseActivity
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_youtube);
+        //setContentView(R.layout.activity_youtube);
+        //ConstraintLayout constraintLayout = (ConstraintLayout) findViewById(R.id.activity_youtube);
+        ConstraintLayout layout = (ConstraintLayout) getLayoutInflater().inflate(R.layout.activity_youtube, null);
+        setContentView(layout);
+
+        YouTubePlayerView player = new YouTubePlayerView(this);
+        player.setLayoutParams(new ConstraintLayout.LayoutParams(ViewGroup.LayoutParams.MATCH_PARENT, ViewGroup.LayoutParams.MATCH_PARENT));
+        player.addView(player);
     }
 
     @Override
